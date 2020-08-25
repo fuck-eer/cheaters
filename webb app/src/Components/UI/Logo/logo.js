06cade77c376f71ba9f34e85a633dbd505e0eecc
fuck-eer <nishchaychauhan8@gmail.com>
2020-08-26 03:39:44 +0530

    logo add

---
+++ webb app/src/Components/UI/Logo/logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../../../assets/Logo/logo.png'
+import '../Navbar/Navbar.css'
+
+const Logofile =()=>(
+    <div className='logo'>
+        <img src={Logo} alt='Cheat-It' />
+    </div>
+)
+
+export default Logofile
--- webb app/src/Components/UI/Navbar/Navbar.js
+++ webb app/src/Components/UI/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
+import Logofile from '../Logo/logo'
 const nav =()=>{
 return(
     <header>
          <nav>
+         <Logofile className='logo' />
+
              <ul>
                 <NavLink to='/database' exact className='dat' activeClassName='activist'>Database</NavLink>
                 <NavLink to='/stats' exact className='dat' activeClassName='activist'>Statistics</NavLink>
                 <NavLink to='/findit' exact className='dat' activeClassName='activist'>Quick Find</NavLink>
+
              </ul>
+
          </nav>
 
     </header>
